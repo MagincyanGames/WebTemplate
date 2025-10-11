0106e11f54db3f6b6456ab179b3176b2f563aaf1
magin <maginciancompany@gmail.com>
2025-10-11 16:20:16 +0200

    Init template

--- src/App.tsx
+++ src/App.tsx
@@ -20,12 +20,14 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <button onClick={() => setCount((count) => count + 1)}>
+          {t('count-text')} {count}
+        </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          {t('text.edit')} <code>src/App.tsx</code> {t('text.save_hmt')}
         </p>
       </div>
-      <p className='read-the-docs'>{t('example')}</p>
+      <p className='read-the-docs'>{t('footer')}</p>
     </>
   )
 }
